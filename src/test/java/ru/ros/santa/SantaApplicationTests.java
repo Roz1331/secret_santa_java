c71adb3636076e682db303b365f3073205f5de51
Roz1331 <ismailova_roza_1@mail.ru>
2022-11-05 22:57:49 +0300

    initial commit

---
+++ src/test/java/ru/ros/santa/SantaApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ros.santa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
